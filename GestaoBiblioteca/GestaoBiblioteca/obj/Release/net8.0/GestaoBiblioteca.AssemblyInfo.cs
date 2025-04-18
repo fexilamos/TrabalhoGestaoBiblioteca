fc9903ac15756ece8f9fe5f7d2a5c164e58b5a35
fexilamos <tiago.cardona@gmail.com>
2025-04-18 21:05:24 +0100

    introducao de writeline.clearterminei os metodos nas classes, criei o main para correr o código, está funcional mas ainda necessita de muitos ajustes.

--- GestaoBiblioteca/GestaoBiblioteca/obj/Release/net8.0/GestaoBiblioteca.AssemblyInfo.cs
+++ GestaoBiblioteca/GestaoBiblioteca/obj/Release/net8.0/GestaoBiblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fe11932567572a91e1ead65a035fbc6bd060da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b94b89e8693ccc80f235dba946e9f35f74924c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
